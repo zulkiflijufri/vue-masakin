5b719e5f550d5cbe56373ed3ea88563871dd409a
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-12 20:03:56 +0800

    Refactor app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <Navbar />
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
 
 export default {
-  name: "App",
-  components: {
-    Navbar,
-  },
+    name: "App",
+    components: {
+        Navbar,
+    },
 };
 </script>
