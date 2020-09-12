538d154beac7415aae40e552d5ba6d3c83ee83d6
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-12 22:23:28 +0800

    Add component footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="mt-5 mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <strong>&copy; 2020 Makassar ❤</strong>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+};
+</script>
