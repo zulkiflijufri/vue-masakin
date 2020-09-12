e1e2117cab00b3c1886c119505aa01b61cd620c6
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-12 22:25:25 +0800

    Finishing touch

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,22 +1,48 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-md-6 d-flex">
-                <div class="justify-content-center align-self-center">
-                    <h2>
-                        <strong>Nikmati Menu Makanan</strong>
-                        <br />hanya dengan sekali klik
-                    </h2>
-                    <p class="mt-3" style="font-size:20px;">
-                        Pesan segera makanan favorit Kamu
-                    </p>
-                    <b-button variant="info" class="btn-pesan" to="/foods"
-                        >Pesan</b-button
-                    >
+        <!-- Desktop -->
+        <div class="d-none d-md-block">
+            <div class="row">
+                <div class="col-md-6 d-flex">
+                    <div class="justify-content-center align-self-center">
+                        <h2>
+                            <strong>Nikmati Menu Makanan</strong>
+                            <br />hanya dengan sekali klik
+                        </h2>
+                        <p class="mt-3" style="font-size:20px;">
+                            Pesan segera makanan favorit Kamu
+                        </p>
+                        <b-button variant="info" class="btn-pesan" to="/foods">
+                            Pesan
+                        </b-button>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-5">
+                    <img src="../assets/img/food.png" alt="Hero" width="100%" />
                 </div>
             </div>
-            <div class="col-md-6 mt-5">
-                <img src="../assets/img/food.png" alt="Hero" width="100%" />
+        </div>
+
+        <!-- Mobile -->
+        <div class="d-sm-block d-md-none">
+            <div class="row">
+                <div class="col-md-6 mt-2">
+                    <img src="../assets/img/food.png" alt="Hero" width="100%" />
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="justify-content-center align-self-center">
+                        <h2 style="font-size: 34px" class="mt-3">
+                            <strong>Nikmati Menu Makanan</strong>
+                            <br />hanya dengan sekali klik
+                        </h2>
+                        <p class="mt-3" style="font-size:20px;">
+                            Pesan segera makanan favorit Kamu
+                        </p>
+                        <b-button variant="info" class="btn-pesan" to="/foods"
+                            >Pesan</b-button
+                        >
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,48 @@
 <template>
-    <div class="container">
+    <div class="home">
         <Hero />
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h2>Best<strong> Foods</strong></h2>
+                </div>
+            </div>
+            <div class="row mt-1 mb-5">
+                <div
+                    class="col-md-4"
+                    v-for="product in products"
+                    :key="product.id"
+                >
+                    <CardProduct :product="product" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Hero from "../components/Hero";
+import CardProduct from "../components/CardProduct";
+import axios from "axios";
+
 export default {
     name: "Home",
     components: {
         Hero,
+        CardProduct,
+    },
+    data() {
+        return {
+            products: [],
+        };
+    },
+    created() {
+        axios
+            .get("http://localhost:3000/best_products")
+            .then(res => {
+                this.products = res.data;
+            })
+            .catch(err => console.log(err));
     },
 };
 </script>
