c6ed2d3efb21e0b19a3c732f1d754901181faa98
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-12 17:54:34 +0800

    Add router

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Home from "../views/Home";
+import Foods from "../views/Foods";
+
+Vue.use(VueRouter);
+
+const routes = [
+    { path: "/", component: Home },
+    { path: "/foods", component: Foods },
+];
+
+const router = new VueRouter({
+    routes,
+    mode: "history",
+});
+
+export default router;
