488569a765d71b4f111b66f241fcd5378caab8be
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-12 17:54:52 +0800

    Add views

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <!-- <div> -->
+    <h1>Home</h1>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
