57e5e382616c2a08ebb81a1de87c70e90e336635
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-14 21:36:57 +0800

    Validation order

--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -13,27 +13,42 @@
                 <br />
                 <h4>Price: Rp. {{ product.harga }}</h4>
                 <br />
-                <b-form-group label-cols="4" label-cols-lg="2" label="Qty" label-for="input-sm">
-                    <b-form-input id="input-sm" size="sm"></b-form-input>
-                </b-form-group>
-                <b-form-group
-                    label-cols="4"
-                    label-cols-lg="2"
-                    label="Information"
-                    label-for="input-sm"
-                >
-                    <b-form-textarea
-                        id="textarea-no-resize"
-                        placeholder="ex: hot, cool, etc"
-                        rows="2"
-                        no-resize
-                    ></b-form-textarea>
-                </b-form-group>
-                <b-form-group>
-                    <b-button :to="'/food/'+product.id" variant="info" class="btn-block">
-                        ORDER
-                    </b-button>
-                </b-form-group>
+                <div class="form-order">
+                    <b-form-group label-cols="4" label-cols-lg="2" label="Qty" label-for="input-sm">
+                        <b-form-input
+                            id="input-sm"
+                            size="sm"
+                            autocomplete="off"
+                            v-model="order.qty"
+                            @keyup="cekQty"
+                            type="number"
+                        ></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                        label-cols="4"
+                        label-cols-lg="2"
+                        label="Information"
+                        label-for="input-sm"
+                    >
+                        <b-form-textarea
+                            id="textarea-no-resize"
+                            placeholder="ex: hot, cool, etc"
+                            rows="2"
+                            no-resize
+                            autocomplete="off"
+                            v-model="order.information"
+                        ></b-form-textarea>
+                    </b-form-group>
+                    <b-form-group>
+                        <b-button
+                            :to="'/food/'+product.id"
+                            variant="info"
+                            class="btn-block"
+                            @click="Order"
+                            :style="{opacity: btnOpacity, pointerEvents: btnPointerEvents }"
+                        >ORDER</b-button>
+                    </b-form-group>
+                </div>
             </div>
         </div>
     </div>
@@ -45,7 +60,38 @@ export default {
     data() {
         return {
             product: [],
+            order: {
+                qty: "",
+                information: "",
+            },
+            btnOpacity: "0.4",
+            btnPointerEvents: "none",
         };
+    },
+    methods: {
+        Order() {
+            this.order.products = this.product;
+            axios
+                .post("http://localhost:3000/keranjangs/", this.order)
+                .then(() => {
+                    this.$toast.open({
+                        type: "success",
+                        position: "top",
+                        duration: 3000,
+                        message: "Food successfully to order.",
+                    });
+                })
+                .catch((err) => console.log(err));
+        },
+        cekQty() {
+            if (this.order.qty !== "") {
+                this.btnOpacity = "1";
+                this.btnPointerEvents = "";
+            } else {
+                this.btnOpacity = "0.4";
+                this.btnPointerEvents = "none";
+            }
+        },
     },
     created() {
         axios
