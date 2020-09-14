761da6d23ac58fbf56c4e844695fe23fde9a7c0b
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-14 21:36:13 +0800

    Config toast notification

--- src/main.js
+++ src/main.js
@@ -9,9 +9,15 @@ import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./assets/css/main.css";
+import VueToast from "vue-toast-notification";
+// Import one of available themes
+import "vue-toast-notification/dist/theme-default.css";
+//import 'vue-toast-notification/dist/theme-sugar.css';
 
+Vue.use(VueToast);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.use(VueToast);
 
 new Vue({
     router,
