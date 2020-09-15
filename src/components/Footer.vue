8bfc8048957faf6d0a1108e199246846cebef104
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-15 16:39:04 +0800

    Finishing touch footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col text-center">
-                    <strong>Kuliner, Makassar. &copy; 2020</strong>
+                    <span>Kuliner, Makassar. &copy; 2020</span>
                 </div>
             </div>
         </div>
