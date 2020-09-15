07af3e936b09b0e5e3b4bb0a7acb23d0a3ed4f22
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-15 19:00:51 +0800

    Add view orders

---
+++ src/views/Orders.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col text-center">
+                <p>Orders</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
