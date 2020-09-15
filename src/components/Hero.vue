d1793774f5f1d37837acd27b5b2ad3f7f35351d3
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-15 17:03:38 +0800

    Finishing touch hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -10,7 +10,6 @@
                             <br />hanya dengan sekali klik
                         </h2>
                         <p class="mt-3" style="font-size:20px;">Pesan segera makanan favorit Kamu</p>
-                        <b-button variant="info" class="btn-pesan" router-link to="/foods">Pesan</b-button>
                     </div>
                 </div>
                 <div class="col-md-6 mt-5">
@@ -32,7 +31,6 @@
                             <br />hanya dengan sekali klik
                         </h2>
                         <p class="mt-3" style="font-size:20px;">Pesan segera makanan favorit Kamu</p>
-                        <b-button variant="info" class="btn-pesan mb-4" to="/foods">Pesan</b-button>
                         <hr />
                     </div>
                 </div>
