8a2669f30340c6bf74dec39871bd0c7a50bed0ca
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-16 23:21:14 +0800

    Finishing touch

---
+++ src/views/Success.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <Navbar />
+        <div class="row justify-content-center mb-5 mt-5">
+            <div class="col text-center">
+                <img src="../assets/img/success.png" width="65%" class="mb-5">
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+export default {
+    components: {
+        Navbar,
+        Footer
+    }
+};
+</script>
+
+<style>
+</style>
